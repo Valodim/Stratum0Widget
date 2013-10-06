f1a6a96bd86ba087aad6cb6f018e8eaa82121690
Matthias Uschok <dev@uschok.de>
2013-10-06 18:07:45 +0200

    Removed unused imports

--- src/main/java/org/stratum0/statuswidget/SpaceStatus.java
+++ src/main/java/org/stratum0/statuswidget/SpaceStatus.java
@@ -1,24 +1,6 @@
 package org.stratum0.statuswidget;
 
-import android.os.AsyncTask;
-import android.util.Log;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-
-import static org.stratum0.statuswidget.GlobalVars.TAG;
-import static org.stratum0.statuswidget.GlobalVars.url;
 
 /**
  * Created by tsuro on 9/1/13.
--- src/main/java/org/stratum0/statuswidget/StratumsphereStatusProvider.java
+++ src/main/java/org/stratum0/statuswidget/StratumsphereStatusProvider.java
@@ -2,15 +2,11 @@ package org.stratum0.statuswidget;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONObject;
 
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
