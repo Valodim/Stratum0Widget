d8475a4928cbccb448e9f1c860161de808b3ae3e
Matthias Uschok <dev@uschok.de>
2013-11-18 17:39:50 +0100

    Change task now tries up to 20 times to change the status

--- src/main/java/org/stratum0/statuswidget/SpaceStatusChangeTask.java
+++ src/main/java/org/stratum0/statuswidget/SpaceStatusChangeTask.java
@@ -1,6 +1,6 @@
 package org.stratum0.statuswidget;
 
-import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
@@ -8,46 +8,96 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 import static org.stratum0.statuswidget.GlobalVars.TAG;
+import static org.stratum0.statuswidget.GlobalVars.setStatusAttempts;
+import static org.stratum0.statuswidget.GlobalVars.setStatusUrl;
 
 /**
- * Created by matthias on 10/6/13.
+ * Created by Matthias Uschok <dev@uschok.de> on 2013-10-06.
  */
 public class SpaceStatusChangeTask extends AsyncTask <String, Void, Void> {
 
-    private Activity caller;
+    private SpaceStatus status;
+    private SpaceStatus.Status currentStatus, requestedStatus;
+    private ArrayList<SpaceStatusListener> receiverList = new ArrayList<SpaceStatusListener>();
+    private Context context;
 
-    public SpaceStatusChangeTask(Activity caller) {
-        this.caller = caller;
+    public SpaceStatusChangeTask(Context context) {
+        this.context = context;
+        status = SpaceStatus.getInstance();
     }
 
     @Override
     protected Void doInBackground(String... strings) {
 
+        String updateUrl = setStatusUrl + strings[0];
+        if (strings[0].contains("open")) {
+            requestedStatus = SpaceStatus.Status.OPEN;
+        }
+        else if (strings[0].contains("close")) {
+            requestedStatus = SpaceStatus.Status.CLOSED;
+        }
+
+        currentStatus = status.getStatus();
         try {
-            URL u = new URL(strings[0]);
-            URLConnection c = u.openConnection();
-            c.connect();
-            c.getContent();
-            Thread.sleep(1000);
+            for (int i = 0; i < setStatusAttempts; i++) {
+                URL u = new URL(updateUrl);
+                URLConnection c = u.openConnection();
+                c.connect();
+                c.getContent();
+                Thread.sleep(1000);
+                SpaceStatusUpdateTask updateTask = new SpaceStatusUpdateTask(null);
+                updateTask.execute();
+                currentStatus = updateTask.get();
+                if (currentStatus == requestedStatus) {
+                    break;
+                }
+                else {
+                    URL ircstatus = new URL(setStatusUrl + "Status%20change%20failed.%20Trying%20again...%20%28" + (i+1) + "%20of%20" + setStatusAttempts + "%29");
+                    URLConnection irc = ircstatus.openConnection();
+                    irc.connect();
+                    irc.getContent();
+                }
+            }
         } catch (MalformedURLException e) {
             Log.e(TAG, "Update request: malformed URL.", e);
         } catch (IOException e) {
             Log.e(TAG, "Update request: could not connect to server.", e);
         } catch (InterruptedException e) {
             Log.e(TAG, "Wait for new status didn't finish:", e);
+        } catch (ExecutionException e) {
+            Log.e(TAG, "Error executing update task inside change task:", e);
         }
 
         return null;
     }
 
     @Override
-    protected void onPostExecute(Void aVoid) {
-        super.onPostExecute(aVoid);
+    protected void onPreExecute() {
+        super.onPreExecute();
+        for (SpaceStatusListener receiver : receiverList) {
+            receiver.onPreSpaceStatusUpdate(context);
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Void avoid) {
+        super.onPostExecute(avoid);
+        for (SpaceStatusListener receiver : receiverList) {
+            receiver.onPostSpaceStatusUpdate(context);
+        }
+    }
 
         SpaceStatusUpdateTask updateTask = new SpaceStatusUpdateTask(caller);
         updateTask.addListener((SpaceStatusListener) caller);
         updateTask.execute();
     }
+
+    public void addListener(SpaceStatusListener receiver) {
+        receiverList.add(receiver);
+    }
+
 }
