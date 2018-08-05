e5ba1115c25cc486ef7803eb27393048f4d7d34b
Vincent Breitmoser <look@my.amazin.horse>
2018-08-05 13:21:44 +0200

    ask wifi pwd for auth, instead of location services

---
+++ app/src/main/java/horse/amazin/my/stratum0/statuswidget/interactors/S0PermissionManager.kt
@@ -0,0 +1,29 @@
+package horse.amazin.my.stratum0.statuswidget.interactors
+
+import android.content.Context
+import okio.ByteString
+import java.security.MessageDigest
+
+object S0PermissionManager {
+    fun maySetSpaceStatus(context: Context): Boolean {
+        val preferences = context.getSharedPreferences("preferences", Context.MODE_PRIVATE)
+
+        return preferences.getBoolean("wasAtS0Before", false) ||
+                preferences.getBoolean("spottedS0Wifi", false) ||
+                preferences.getBoolean("allowSetSpaceStatus", false)
+    }
+
+    fun allowSetSpaceStatus(context: Context) {
+        val preferences = context.getSharedPreferences("preferences", Context.MODE_PRIVATE)
+
+        preferences.edit().putBoolean("allowSetSpaceStatus", true).apply()
+    }
+
+    fun isS0WifiPwd(pwd: ByteArray): Boolean {
+        val messageDigest = MessageDigest.getInstance("SHA-256")
+        val pwdDigest = messageDigest.digest(pwd)
+        return pwdDigest.contentEquals(S0WIFISHA256)
+    }
+
+    private val S0WIFISHA256 = ByteString.decodeHex("b2f34b2eb4fb32ca1928016d9d502d4e9d88ba0730e2a24eca3df7601be450b6").toByteArray()
+}
